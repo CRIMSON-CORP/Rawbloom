cac39a4373f95875bd13c31be35272130ee934ca
Oluwatowo <olueatoworosanwo@gmail.com>
2021-03-05 20:34:58 +0100

    Major UI changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -114,19 +114,6 @@ function Footer() {
                     </div>
                 </div>
             </div>
-
-            <div className="container">
-                <div className="copyright">
-                    &copy; Copyright{" "}
-                    <strong>
-                        <span>Restaurantly</span>
-                    </strong>
-                    . All Rights Reserved
-                </div>
-                <div className="credits">
-                    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-                </div>
-            </div>
         </footer>
     );
 }
